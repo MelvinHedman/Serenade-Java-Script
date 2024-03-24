d49bcc94a2d18d7d7b5368113508ec0b6a45c75f
Melvin Hedman <mehe2100@student.miun.se>
2024-03-24 10:32:59 +0100

    changes

--- custom.js
+++ custom.js
@@ -1,43 +1,61 @@
-/* Serenade Custom Commands
 
-In this file, you can define your own custom commands with the Serenade API.
-
-For instance, here's a custom automation that opens your terminal and runs a command:
-
-serenade.global().command("make", api => {
-  api.focusApplication("terminal");
-  api.typeText("make clean && make");
-  api.pressKey("return");
-});
-
-And, here's a Python snippet for creating a test method:
-
-serenade.language("python").snippet(
-  "test method <%identifier%>",
-  "def test_<%identifier%>(self):<%newline%><%indent%>pass",
-  { "identifier": ["underscores"] }
-  "method"
-);
-
-For more information, check out the Serenade API documentation: https://serenade.ai/docs/api
-
-*/
-
-
-// serenade.global().command("new file <%filename%>", api => {
-//   api.typeText("AFIFNAIFNAI")
-// });
+const newFileCommand = "pageup";
+const newDirectoryCommand = "pagedown"
 
 serenade.global().command("new class <%classname%>", async (api, matches) => {
-  await api.pressKey("pageup", ["alt", "control"]);
-  await api.typeText(matches.classname)
+  await api.pressKey(newFileCommand, ["alt", "control"]);
+  classname = matches.classname.charAt(0).toUpperCase() + matches.classname.slice(1);
+  await api.typeText(classname)
   await api.pressKey("enter", [])
+  await api.pressKey("", ["control", "right"])
+  await api.pressKey("enter", [])
+});
+
+serenade.global().command("new abstract class <%classname%>", async (api, matches) => {
+  await api.pressKey(newFileCommand, ["alt", "control"]);
+  classname = matches.classname.charAt(0).toUpperCase() + matches.classname.slice(1);
+  await api.typeText(classname)
+  await api.pressKey("enter", [])
+  await convertClassToAbstract(api, matches)
+  await api.pressKey("", ["control", "right"])
+  await api.pressKey("enter", [])
+});
+
+serenade.global().command("tooltip", async (api) => {
+  await api.pressKey("", ["alt", "enter"]);
+});
+
+async function convertClassToAbstract(api, matches){
+  await api.delay(50)
+  await api.pressKey("r", ["ctrl", "shift"])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.delay(50)
+  classname = matches.classname.charAt(0).toUpperCase() + matches.classname.slice(1);
+  await api.delay(50)
+  await api.typeText("class " + classname)
+  await api.pressKey("tab", [])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.typeText("abstract class " + classname)
+  await api.pressKey("enter", [])
+  await api.pressKey("escape", [])
+}
+
+serenade.global().command("convert class <%classname%> abstract", async (api, matches) => {
+  await convertClassToAbstract(api, matches)
+  await api.pressKey("enter", ["control", "right"])
 });
 
 serenade.global().command("new package <%packagename%>", async (api, matches) => {
-  await api.pressKey("pagedown", ["alt", "control"]);
+  await api.pressKey(newDirectoryCommand, ["alt", "control"]);
   await api.typeText(matches.packagename)
   await api.pressKey("enter", [])
+
+})
+
+serenade.global().command("file select", async (api, matches) => {
+  await api.pressKey("1", ["alt"]);
 })
 
 
