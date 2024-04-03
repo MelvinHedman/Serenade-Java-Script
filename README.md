b0408e12784d9442456701b5eee698fe339d6ca1
Melvin Hedman <mehe2100@student.miun.se>
2024-04-03 14:17:40 +0200

    changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Serenade Custom Script for Java
+
+## Commands
+| Command     | Description |
+| ----------- | ----------- |
+| Tooltip     | Opens tooltip (ALT + Enter)       |
+| File Select   | Selects the current file        |
+| New package \<name>   | Creates new package \<name> in current/selected directory        |
+| New class \<name>    | Creates new class \<name>  in current/selected directory        |
+| New class \<name>  extends \<name2>    | Makes new class \<name> that extends \<name2>        |
+| New class \<name> implements \<name2>   | Creates class \<name> that implements \<name2>        |
+| New abstract class \<name>  | Creates new abstract class \<name>  in current/selected directory        |
+| New interface \<name>   | Makes new interface \<name> in current/selected directory        |
+| Convert class \<name>  abstract   | Makes the class abstract |
+| Extend class \<name> with \<name2>   | Extends class \<name> with \<name2>        |
+| new enum \<name>   | Creates new ENUM \<name> in current/selected directory        |
+| ...   | ...        |
--- custom.js
+++ custom.js
@@ -42,11 +42,13 @@ serenade.app("JetBrains").command("new abstract class <%classname%>", async (api
 
 // Create an object
 
+
 // Define constrtuctor in a class
 // _--_ Finns i vanliga serenade en smidig?
 
 
 // Add private members to a class and public getter and setter methods
+// Tooltip?
 
 // Make a class abstract
 async function convertClassToAbstract(api, matches){
@@ -75,13 +77,67 @@ serenade.app("JetBrains").command("convert class <%classname%> abstract", async 
 
 
 // Create a class that inherits from superclass
+async function extendClass(api, matches) {
+  await api.delay(200)
+  await api.pressKey("r", ["ctrl", "shift"])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.delay(200)
+  superclassname = matches.superclass.charAt(0).toUpperCase() + matches.superclass.slice(1);
+  await api.delay(200)
+  await api.typeText("class " + classname)
+  await api.pressKey("tab", [])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.typeText("class " + classname + " extends " + superclassname)
+  await api.delay(200)
+  await api.pressKey("enter", [])
+  await api.delay(200)
+  
+}
+
+async function implementClass(api, matches) {
+  await api.delay(200)
+  await api.pressKey("r", ["ctrl", "shift"])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.delay(200)
+  interfacename = matches.interfacename.charAt(0).toUpperCase() + matches.interfacename.slice(1);
+  await api.delay(200)
+  await api.typeText("class " + classname)
+  await api.pressKey("tab", [])
+  await api.pressKey("a", ["ctrl"])
+  await api.pressKey("delete", [])
+  await api.typeText("class " + classname + " implements " + interfacename)
+  await api.delay(200)
+  await api.pressKey("enter", [])
+  await api.delay(200)
+}
+
+serenade.app("JetBrains").command("new class <%classname%> extends <%superclass%>" , async (api, matches) => {
+  await api.pressKey(newFileCommand, ["alt", "control"]);
+  classname = matches.classname.charAt(0).toUpperCase() + matches.classname.slice(1);
+  await api.typeText(classname)
+  await api.pressKey("enter", [])
+  await extendClass(api, matches)
+  await api.pressKey("escape", [])
+  await api.pressKey("", ["control", "right"])
+  await api.pressKey("enter", [])
+});
+
+serenade.app("JetBrains").command("extend class <%classname%> with <%superclass%>", async (api, matches) => {
+  await extendClass(api, matches)
+  await api.pressKey("enter", ["control", "right"])
+});
 
 // Define two abstract methods in an abstract class
+// Finns väldigt bra "create function public abstract voice makeSound tex."
 
 // Override two methods in a subclass of an abstract class
+// Använda tooltip? 
+
 
 // Define an interface
-
 // Implement Interface
 serenade.app("JetBrains").command("new interface <%classname%>", async (api, matches) => {
   await api.pressKey(newFileCommand, ["alt", "control"]);
@@ -94,7 +150,19 @@ serenade.app("JetBrains").command("new interface <%classname%>", async (api, mat
   await api.pressKey("enter", [])
 });
 
-// User superclass reference to refer to a subclass object and calling overridden methods
+serenade.app("JetBrains").command("new class <%classname%> implements <%interfacename%>" , async (api, matches) => {
+  await api.pressKey(newFileCommand, ["alt", "control"]);
+  classname = matches.classname.charAt(0).toUpperCase() + matches.classname.slice(1);
+  await api.typeText(classname)
+  await api.pressKey("enter", [])
+  await implementClass(api, matches)
+  await api.pressKey("escape", [])
+  await api.pressKey("", ["control", "right"])
+  await api.pressKey("enter", [])
+});
+
+// Use superclass reference to refer to a subclass object and calling overridden methods
+// Använda tooltip?
 
 // Add a static method in a class
 
@@ -124,3 +192,25 @@ serenade.app("JetBrains").command("new enum <%classname%>", async (api, matches)
 
 // Use a collection to store, retrieve and manipulate a group of objects. 
 
+serenade.app("JetBrains").command("set value of <%type%> <%variable%> to <%value%>", async (api, matches) => {
+  await api.pressKey("f", ["ctrl"])
+  await api.delay(100);
+  await api.typeText(matches.type + " " + matches.variable);
+  await api.delay(100);
+  await api.pressKey("enter", [])
+  await api.delay(100);
+  await api.pressKey("escape", [])
+  await api.delay(100);
+  await api.pressKey("right", ["ctrl"])
+  await api.delay(100);
+  await api.pressKey("right", ["ctrl"])
+  await api.delay(100);
+  await api.pressKey("right", ["ctrl", "shift"])
+  await api.delay(100);
+  await api.pressKey("delete", [])
+  await api.delay(100);
+  await api.typeText(matches.value);
+});
+
+
+// Create constructor
